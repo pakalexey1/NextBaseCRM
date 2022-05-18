6bd95b574238aa9e1fffe85b9b9998bdc265d505
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-05-18 11:19:27 -0400

    first test testng second test added

--- src/test/java/com/nextbasecrm/test/practice1/TS011test.java
+++ src/test/java/com/nextbasecrm/test/practice1/TS011test.java
@@ -9,20 +9,22 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TS011test {
+
+    final String USERNAME1 = "helpdesk1@cybertekschool.com";
+    final String USERNAME2 = "helpdesk2@cybertekschool.com";
+    final String PASSWORD = "UserUser";
+
     @Test
-    public void testTS011 (){
+    public void testTS011_1 (){
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
-        final String USERNAME = "helpdesk1@cybertekschool.com";
-        final String EMAILADDRESS = "helpdesk1@cybertekschool.com";
-        final String PASSWORD = "UserUser";
 
         driver.get("https://login.nextbasecrm.com/");
         WebElement usernameBox = driver.findElement(By.name("USER_LOGIN"));
-        usernameBox.sendKeys(USERNAME);
+        usernameBox.sendKeys(USERNAME1);
 
         WebElement passwordBox = driver.findElement(By.name("USER_PASSWORD"));
         passwordBox.sendKeys(PASSWORD);
@@ -48,5 +50,38 @@ public class TS011test {
 
         driver.quit();
 
+    }
+
+    @Test
+    public void testTS011_2 (){
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://login.nextbasecrm.com/");
+        WebElement usernameBox = driver.findElement(By.name("USER_LOGIN"));
+        usernameBox.sendKeys(USERNAME2);
+
+        WebElement passwordBox = driver.findElement(By.name("USER_PASSWORD"));
+        passwordBox.sendKeys(PASSWORD);
+
+        WebElement loginButton = driver.findElement(By.className("login-btn"));
+        loginButton.click();
+
+        String expectedHeader1 = "CRM";
+        String expectedHeader2 = "24";
+
+        WebElement header1 = driver.findElement(By.xpath("//a[@id='logo_24_a']/span/span[@class='logo-text']"));
+        String actualHeader1 = header1.getText();
+
+        WebElement header2 = driver.findElement(By.xpath("//a[@id = 'logo_24_a']/span/span[@class='logo-color']"));
+
+        String actualHeader2 = header2.getText();
+
+        Assert.assertEquals(expectedHeader1, actualHeader1);
+        Assert.assertEquals(expectedHeader2, actualHeader2);
+
+        driver.quit();
     }
 }
