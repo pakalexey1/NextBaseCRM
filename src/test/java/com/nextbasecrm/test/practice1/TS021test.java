6e68c117a331d42c4576b278e9b68d9166eb4d71
Veronica Pak <nikampak@gmail.com>
2022-05-18 11:30:25 -0400

    Test Case 021 finished

--- src/test/java/com/nextbasecrm/test/practice1/TS021test.java
+++ src/test/java/com/nextbasecrm/test/practice1/TS021test.java
@@ -1,23 +1,43 @@
 package com.nextbasecrm.test.practice1;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TS021test {
     @Test
-    public void testTS021(){
+    public void testTS021() throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
+        final String emailAddress = "helpdesk1@cybertekschool.com";
+
+        driver.get("https://login.nextbasecrm.com/");
+        WebElement forgotPassword = driver.findElement(By.className("login-link-forgot-pass"));
+        forgotPassword.click();
+
+        WebElement emailAddressBox = driver.findElement(By.name("USER_EMAIL"));
+        emailAddressBox.sendKeys(emailAddress);
+
+        WebElement resetPasswordButton = driver.findElement(By.className("login-btn"));
+        resetPasswordButton.click();
+
+        WebElement noteTextMsg = driver.findElement(By.className("notetext"));
 
 
+        String actualNoteTextMsg = noteTextMsg.getText();
+        String expectedNoteTextMsg = "A code to reset your password and your registration information has just been " +
+                "sent to your e-mail address. Please check your e-mail. Note that the reset code is re-generated on each request.";
+        Assert.assertEquals(actualNoteTextMsg,expectedNoteTextMsg);
 
 
-        driver.quit();
+//        driver.quit();
 
     }
 }
