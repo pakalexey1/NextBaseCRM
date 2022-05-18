cdd8bcdc90de6bf682c2b773e43a6b3d2f596e2f
Veronica Pak <nikampak@gmail.com>
2022-05-18 10:25:52 -0400

    Initial commit

---
+++ src/test/java/com/nextbasecrm/test/TS011test.java
@@ -0,0 +1,47 @@
+package com.nextbasecrm.test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TS011test {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        final String USERNAME = "helpdesk1@cybertekschool.com";
+        final String EMAILADDRESS = "helpdesk1@cybertekschool.com";
+        final String PASSWORD = "UserUser";
+
+        driver.get("https://login.nextbasecrm.com/");
+        WebElement usernameBox = driver.findElement(By.name("USER_LOGIN"));
+        usernameBox.sendKeys(USERNAME);
+
+        WebElement passwordBox = driver.findElement(By.name("USER_PASSWORD"));
+        passwordBox.sendKeys(PASSWORD);
+
+        WebElement loginButton = driver.findElement(By.className("login-btn"));
+        loginButton.click();
+
+        String expectedHeader1 = "CRM";
+        String expectedHeader2 = "24";
+
+        WebElement header1 = driver.findElement(By.xpath("//a[@id='logo_24_a']/span/span[@class='logo-text']"));
+        String actualHeader1 = header1.getText();
+
+        WebElement header2 = driver.findElement(By.xpath("//a[@id = 'logo_24_a']/span/span[@class='logo-color']"));
+        String actualHeader2 = header2.getText();
+
+        System.out.println("Expected Header1 Test passed: " + expectedHeader1.equals(actualHeader1));
+        System.out.println("Expected Header2 Test passed: " + expectedHeader2.equals(actualHeader2));
+
+
+
+
+        driver.quit();
+    }
+}
