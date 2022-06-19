d35c2818c5feebfd9b55f81eb5d7e18b6766acfc
AlexaSky <alexasky111@gmail.com>
2022-06-19 11:41:11 -0400

    created configuration.properties to avoid hardcoding in the project

--- src/test/java/com/nextbasecrm/test/utilities/ConfigurationReader.java
+++ src/test/java/com/nextbasecrm/test/utilities/ConfigurationReader.java
@@ -1,5 +1,36 @@
 package com.nextbasecrm.test.utilities;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigurationReader {
+
+    //1- Create Properties class object (coming from Java, helps to reach to configuration.properties)
+    private static Properties properties = new Properties();
+
+    //running 1st, doing it ONE time
+    //need static since static have to be initialized in static block ONLY
+    static {
+
+        try {
+    //2- We need to open the file in the Java memory
+            FileInputStream file = new FileInputStream("configuration.properties");
+            //now we have to load this file
+    //3- We need to load the "properties" object with the fileStream
+            properties.load(file);
+            //after we have to close this file, it shouldn't be opened, we have to free up the memory
+            file.close();
+        } catch (IOException e) {
+            System.out.println("Error occurred while reading configuration properties file");
+            e.printStackTrace();
+        }
+    }
+    public static String getProperty(String key){
+    //4- Use properties object and getProperty method to pass "key" and read "value"
+        return properties.getProperty(key);
+    }
+
 
 }
--- src/test/java/com/nextbasecrm/test/utilities/Driver.java
+++ src/test/java/com/nextbasecrm/test/utilities/Driver.java
@@ -1,4 +1,62 @@
 package com.nextbasecrm.test.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class Driver {
+    /*
+Q: How are we able to return the same instance of our driver?
+- We implemented Singleton Design Pattern.
+
+Q: What is a design pattern?
+- Repetable solution to a repeating issue.
+
+Q: Singleton Design Pattern:
+- Singleton Design Pattern makes sure we are returning the same instance every time we call our method.
+- In our case we are trying to return the same instance of DRIVER
+     */
+
+    private Driver() {
+        //private - to close access to the object from outside the class
+    }
+
+    private static WebDriver driver; // to close access from outside the class, and static - to be able to access through the Class without creating an obj
+
+    public static WebDriver getDriver() {
+        // created a reusable utility method, which will return same driver instance when we call it
+
+        if (driver == null) { //1st time always null, and it will go inside the if statement
+
+            String browserType = ConfigurationReader.getProperty("browser");
+
+            switch (browserType) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+            }
+        }
+        return driver;
+    }
+
+    public static void closeDriver() {
+
+        if (driver != null) {
+            driver.quit(); //this line terminates the existing session. value will not even be null
+            driver = null;
+        }
+    }
+
 }
